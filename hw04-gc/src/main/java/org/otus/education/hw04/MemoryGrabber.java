feb82198e4dd9f7fe2cd579deca05ff5444df61b
Алексей <codemeric@gmail.com>
2020-08-03 09:45:10 +1000

    change encode file

---
+++ hw04-gc/src/main/java/org/otus/education/hw04/MemoryGrabber.java
@@ -0,0 +1,34 @@
+package org.otus.education.hw04;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class MemoryGrabber {
+
+    public MemoryGrabber() {
+    }
+
+    public void grabAllMemory() throws InterruptedException {
+        List<Object[]> list = new ArrayList<>();
+        while (true){
+            int size = list.size();
+            for (int i = size; i < (size+1000) * 2; i++) {
+                list.add(new Object[100]);
+            }
+            Thread.sleep(100);
+            removeHalf(list);
+        }
+    }
+
+    private void removeHalf(List<Object[]> list) {
+        int i = 0;
+        for (Iterator<Object[]> it = list.iterator(); it.hasNext(); ) {
+            it.next(); // Add this line in your code
+            if (i % 2 == 0) {
+                it.remove();
+            }
+            i++;
+        }
+    }
+}
