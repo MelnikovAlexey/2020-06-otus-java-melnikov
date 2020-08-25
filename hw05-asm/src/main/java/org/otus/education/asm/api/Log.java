a60bec20eef36a4e9b69648434e8d21990e4a12d
Алексей <codemeric@gmail.com>
2020-08-25 10:27:24 +1000

    init commit

---
+++ hw05-asm/src/main/java/org/otus/education/asm/api/Log.java
@@ -0,0 +1,15 @@
+package org.otus.education.asm.api;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(value = RetentionPolicy.RUNTIME)
+@Target(value = ElementType.METHOD)
+public @interface Log {
+
+    String comment() default "";
+
+    boolean printProperty() default true;
+}
