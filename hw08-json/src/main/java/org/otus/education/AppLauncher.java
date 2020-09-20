fab0c7f64cf1a78d4985f9a4e017fbd1533933b0
Алексей <codemeric@gmail.com>
2020-09-20 23:16:35 +1000

    переделал тест,

--- hw08-json/src/main/java/org/otus/education/AppLauncher.java
+++ hw08-json/src/main/java/org/otus/education/AppLauncher.java
@@ -5,11 +5,7 @@ import com.google.gson.Gson;
 import org.otus.education.mson.Mson;
 import org.otus.education.mson.MsonImpl;
 
-import java.util.Collections;
-import java.util.List;
-
 import static java.lang.System.out;
-import static java.lang.System.setOut;
 
 public class AppLauncher {
     public static void main(String[] args) {
@@ -20,14 +16,11 @@ public class AppLauncher {
                 .setStr(null)
                 .setChar('r')
                 .build();
-        Gson gson = new Gson();
-MsonImpl mson = new MsonImpl();
-        String s = gson.toJson((byte) 1);
-        serializeAndPrintStatisticOld(origin);
+
         serializeAndPrintStatistic(origin);
     }
 
-    private static void serializeAndPrintStatisticOld(AnyObject origin) {
+    protected static void serializeAndPrintStatistic(AnyObject origin) {
         out.println("Origin object:");
         out.println(origin.toString());
 
@@ -46,56 +39,7 @@ MsonImpl mson = new MsonImpl();
         out.println("Recreate from Gson String: ");
         out.println(gsonObject.toString());
 
-
         out.println("Result comparison Mson with Origin: " + msonRebuildAny.equals(origin));
         out.println("Result comparison Mson with Gson: " + msonRebuildAny.equals(gsonObject));
-    }
-
-    protected static void serializeAndPrintStatistic(AnyObject origin) {
-        out.println("Origin object:");
-        out.println(origin.toString());
-
-
-        Gson gson = new Gson();
-        var serializer = new MsonImpl();
-
-        if (!gson.toJson(null).equals(serializer.toJson(null))) {
-            throw new RuntimeException("bug0!");
-        }
-        if (!gson.toJson((byte) 1).equals(serializer.toJson((byte) 1))) {
-            throw new RuntimeException("bug1!");
-        }
-        if (!gson.toJson((short) 1f).equals(serializer.toJson((short) 1f))) {
-            throw new RuntimeException("bug2!");
-        }
-        if (!gson.toJson(1).equals(serializer.toJson(1))) {
-            throw new RuntimeException("bug3!");
-        }
-        if (!gson.toJson(1L).equals(serializer.toJson(1L))) {
-            throw new RuntimeException("bug4!");
-        }
-        if (!gson.toJson(1f).equals(serializer.toJson(1f))) {
-            throw new RuntimeException("bug5!");
-        }
-        if (!gson.toJson(1d).equals(serializer.toJson(1d))) {
-            throw new RuntimeException("bug6!");
-        }
-        if (!gson.toJson("aaa").equals(serializer.toJson("aaa"))) {
-            throw new RuntimeException("bug7!");
-        }
-        if (!gson.toJson('a').equals(serializer.toJson('a'))) {
-            throw new RuntimeException("bug8!");
-        }
-        if (!gson.toJson(new int[]{1, 2, 3}).equals(serializer.toJson(new int[]{1, 2, 3}))) {
-            out.println(gson.toJson(new int[]{1, 2, 3}));
-            out.println(serializer.toJson(new int[]{1, 2, 3}));
-            throw new RuntimeException("bug9!");
-        }
-        if (!gson.toJson(List.of(1, 2, 3)).equals(serializer.toJson(List.of(1, 2, 3)))) {
-            throw new RuntimeException("bug10!");
-        }
-        if (!gson.toJson(Collections.singletonList(1)).equals(serializer.toJson(Collections.singletonList(1)))) {
-            throw new RuntimeException("bug11!");
-        }
     }
 }
--- hw08-json/src/test/java/org/otus/education/AppLauncherTest.java
+++ hw08-json/src/test/java/org/otus/education/AppLauncherTest.java
@@ -1,14 +1,39 @@
 package org.otus.education;
 
+import com.google.gson.Gson;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.otus.education.mson.MsonImpl;
 
+import java.util.Collections;
+import java.util.List;
+
+import static java.lang.System.out;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
+@DisplayName("Тестирование своей реализации Json -> Mson")
 class AppLauncherTest {
 
     @Test
     void serializeAndPrintStatistic() {
-        assertDoesNotThrow(()->AppLauncher.serializeAndPrintStatistic(new AnyObject.Builder().build()));
+
+        Gson gson = new Gson();
+        MsonImpl serializer = new MsonImpl();
+
+        assertEquals(gson.toJson(null),serializer.toJson(null),"Bug 1");
+        assertEquals(gson.toJson((byte) 1),serializer.toJson((byte) 1),"Bug 2");
+        assertEquals(gson.toJson((short) 1f),serializer.toJson((short) 1f),"Bug 3");
+        assertEquals(gson.toJson(1),serializer.toJson(1),"Bug 4");
+        assertEquals(gson.toJson(1L),serializer.toJson(1L),"Bug 5");
+        assertEquals(gson.toJson(1f),serializer.toJson(1f),"Bug 6");
+        assertEquals(gson.toJson(1d),serializer.toJson(1d),"Bug 7");
+        assertEquals(gson.toJson("aaa"),serializer.toJson("aaa"),"Bug 8");
+        assertEquals(gson.toJson('a'),serializer.toJson('a'),"Bug 9");
+        assertEquals(gson.toJson(new int[]{1, 2, 3}),serializer.toJson(new int[]{1, 2, 3}),"Bug 10");
+        assertEquals(gson.toJson(List.of(1, 2, 3)),serializer.toJson(List.of(1, 2, 3)),"Bug 11");
+        assertEquals(gson.toJson(Collections.singletonList(1)),serializer.toJson(Collections.singletonList(1)),"Bug 12");
     }
 }
